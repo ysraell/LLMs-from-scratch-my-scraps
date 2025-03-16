9e27168231dcc64d2b2dcc0b14b3a4e701497bcd
Israel de Oliveira <prof.israel@gmail.com>
2025-03-15 21:47:10 -0300

    Initial commit.

---
+++ chap2.py
@@ -0,0 +1,32 @@
+import torch
+import tiktoken
+from torch.utils.data import Dataset, DataLoader
+
+
+# The author's approach stores repeated values massivelly.
+class GPTDatasetLightStorev1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.ids = []
+
+        token_ids = tokenizer.encode(txt)  # 1
+
+        for i in range(0, len(token_ids) - max_length, stride):  # 2
+            chunk = token_ids[i: i + max_length + 1]
+            self.ids.append(torch.tensor(chunk))
+
+    def __len__(self):  # 3
+        return len(self.ids)
+
+    def __getitem__(self, idx):  # 4
+        return self.ids[idx][:-1], self.ids[idx][1:]
+
+
+def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")  # 1
+    # dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)   #2
+    dataset = GPTDatasetLightStorev1(txt, tokenizer, max_length, stride)  # 2
+    dataloader = DataLoader(
+        dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=num_workers  # 3  # 4
+    )
+
+    return dataloader
---
+++ chap4.py
@@ -0,0 +1,101 @@
+from chap3 import MultiHeadAttention
+import torch
+import torch.nn as nn
+
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=1)
+    return idx
+
+
+class FeedForward(nn.Module):
+    def __init__(self, cfg, multi_factor: int = 4):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], multi_factor * cfg["emb_dim"]),
+            nn.GELU(approximate="tanh"),
+            nn.Linear(multi_factor * cfg["emb_dim"], cfg["emb_dim"]),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"],
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"],
+        )
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self, x):
+        # 1
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut  # 2
+
+        shortcut = x  # 3
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut  # 4
+        return x
+
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+
+        self.trf_blocks = nn.Sequential(*[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+
+        self.final_norm = LayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+
+
+# EOF
