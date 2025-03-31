95b8ce590b9b3cf0bad6335b206c4e39c5134b87
Israel de Oliveira <prof.israel@gmail.com>
2025-03-30 21:54:26 -0300

    wip

--- chap5.py
+++ chap5.py
@@ -4,30 +4,32 @@ from chap4 import generate_text_simple
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 
+
 def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
     fig, ax1 = plt.subplots(figsize=(5, 3))
     ax1.plot(epochs_seen, train_losses, label="Training loss")
-    ax1.plot(
-        epochs_seen, val_losses, linestyle="-.", label="Validation loss"
-    )
+    ax1.plot(epochs_seen, val_losses, linestyle="-.", label="Validation loss")
     ax1.set_xlabel("Epochs")
     ax1.set_ylabel("Loss")
     ax1.legend(loc="upper right")
     ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
-    ax2 = ax1.twiny()                   #1
-    ax2.plot(tokens_seen, train_losses, alpha=0)     #2
+    ax2 = ax1.twiny()  # 1
+    ax2.plot(tokens_seen, train_losses, alpha=0)  # 2
     ax2.set_xlabel("Tokens seen")
     fig.tight_layout()
     plt.show()
 
+
 def text_to_token_ids(text, tokenizer):
-    encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
-    encoded_tensor = torch.tensor(encoded).unsqueeze(0)    #1
+    encoded = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)  # 1
     return encoded_tensor
 
+
 def token_ids_to_text(token_ids, tokenizer):
-    flat = token_ids.squeeze(0)                #2
+    flat = token_ids.squeeze(0)  # 2
     return tokenizer.decode(flat.tolist())
+
 
 def assign(left, right):
     if left.shape != right.shape:
@@ -79,117 +81,106 @@ def load_weights_into_gpt(gpt, params):  # 1
     gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
     gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])
 
+
 def generate(model, idx, max_new_tokens, context_size, temperature=0.0, top_k=None, eos_id=None):
-    for _ in range(max_new_tokens):            #1
+    for _ in range(max_new_tokens):  # 1
         idx_cond = idx[:, -context_size:]
         with torch.no_grad():
             logits = model(idx_cond)
         logits = logits[:, -1, :]
-        if top_k is not None:                #2
+        if top_k is not None:  # 2
             top_logits, _ = torch.topk(logits, top_k)
             min_val = top_logits[:, -1]
-            logits = torch.where(
-                logits < min_val,
-                torch.tensor(float('-inf')).to(logits.device),
-                logits
-            )
-        if temperature > 0.0:                  #3
+            logits = torch.where(logits < min_val, torch.tensor(float("-inf")).to(logits.device), logits)
+        if temperature > 0.0:  # 3
             logits = logits / temperature
             probs = torch.softmax(logits, dim=-1)
             idx_next = torch.multinomial(probs, num_samples=1)
-        else:    #4
+        else:  # 4
             idx_next = torch.argmax(logits, dim=-1, keepdim=True)
-        if idx_next == eos_id:              #5
+        if idx_next == eos_id:  # 5
             break
         idx = torch.cat((idx, idx_next), dim=1)
     return idx
 
+
 def calc_loss_batch(input_batch, target_batch, model, device):
-    input_batch = input_batch.to(device)         #1
-    target_batch = target_batch.to(device)      
+    input_batch = input_batch.to(device)  # 1
+    target_batch = target_batch.to(device)
     logits = model(input_batch)
-    loss = torch.nn.functional.cross_entropy(
-        logits.flatten(0, 1), target_batch.flatten()
-    )
+    loss = torch.nn.functional.cross_entropy(logits.flatten(0, 1), target_batch.flatten())
     return loss
 
+
 def calc_loss_loader(data_loader, model, device, num_batches=None):
-    total_loss = 0.
+    total_loss = 0.0
     if len(data_loader) == 0:
         return float("nan")
     elif num_batches is None:
-        num_batches = len(data_loader)     #1
+        num_batches = len(data_loader)  # 1
     else:
-        num_batches = min(num_batches, len(data_loader))   #2
+        num_batches = min(num_batches, len(data_loader))  # 2
     for i, (input_batch, target_batch) in enumerate(data_loader):
         if i < num_batches:
-            loss = calc_loss_batch(
-                input_batch, target_batch, model, device
-            )
-            total_loss += loss.item()    #3
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            total_loss += loss.item()  # 3
         else:
             break
-    return total_loss / num_batches    #4
+    return total_loss / num_batches  # 4
+
 
 def generate_and_print_sample(model, tokenizer, device, start_context):
     model.eval()
     context_size = model.pos_emb.weight.shape[0]
     encoded = text_to_token_ids(start_context, tokenizer).to(device)
     with torch.no_grad():
-        token_ids = generate_text_simple(
-            model=model, idx=encoded,
-            max_new_tokens=50, context_size=context_size
-        )
+        token_ids = generate_text_simple(model=model, idx=encoded, max_new_tokens=50, context_size=context_size)
     decoded_text = token_ids_to_text(token_ids, tokenizer)
-    print(decoded_text.replace("\n", " "))      #1
+    print(decoded_text.replace("\n", " "))  # 1
     model.train()
 
-def train_model_simple(model, train_loader, val_loader,
-                       optimizer, device, num_epochs,
-                       eval_freq, eval_iter, start_context, tokenizer):
-    train_losses, val_losses, track_tokens_seen = [], [], []    #1
+
+def train_model_simple(
+    model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter, start_context, tokenizer
+):
+    train_losses, val_losses, track_tokens_seen = [], [], []  # 1
     tokens_seen, global_step = 0, -1
 
     try:
-        for epoch in range(num_epochs):    #2
+        for epoch in range(num_epochs):  # 2
             model.train()
             for input_batch, target_batch in train_loader:
-                optimizer.zero_grad()   #3
-                loss = calc_loss_batch(
-                    input_batch, target_batch, model, device
-                )
-                loss.backward()                     #4
-                optimizer.step()                    #5
+                optimizer.zero_grad()  # 3
+                loss = calc_loss_batch(input_batch, target_batch, model, device)
+                loss.backward()  # 4
+                optimizer.step()  # 5
                 tokens_seen += input_batch.numel()
                 global_step += 1
-    
-                if global_step % eval_freq == 0:    #6
-                    train_loss, val_loss = evaluate_model(
-                        model, train_loader, val_loader, device, eval_iter)
+
+                if global_step % eval_freq == 0:  # 6
+                    train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
                     train_losses.append(train_loss)
                     val_losses.append(val_loss)
                     track_tokens_seen.append(tokens_seen)
-                    print(f"Ep {epoch+1} (Step {global_step:06d}): "
-                          f"Train loss {train_loss:.3f}, "
-                          f"Val loss {val_loss:.3f}"
+                    print(
+                        f"Ep {epoch+1} (Step {global_step:06d}): "
+                        f"Train loss {train_loss:.3f}, "
+                        f"Val loss {val_loss:.3f}"
                     )
-    
-            generate_and_print_sample(                      #7
-                model, tokenizer, device, start_context
-            )
+
+            generate_and_print_sample(model, tokenizer, device, start_context)  # 7
     except KeyboardInterrupt:
-        print('Process interrupted.')
+        print("Process interrupted.")
     return train_losses, val_losses, track_tokens_seen
 
+
 def evaluate_model(model, train_loader, val_loader, device, eval_iter):
-    model.eval()  #1
-    with torch.no_grad():                              #2
-        train_loss = calc_loss_loader(
-            train_loader, model, device, num_batches=eval_iter
-        )
-        val_loss = calc_loss_loader(
-            val_loader, model, device, num_batches=eval_iter
-        )
+    model.eval()  # 1
+    with torch.no_grad():  # 2
+        train_loss = calc_loss_loader(train_loader, model, device, num_batches=eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
     model.train()
     return train_loss, val_loss
+
+
 # EOF
--- chap6.py
+++ chap6.py
@@ -5,8 +5,8 @@ from pathlib import Path
 import pandas as pd
 import torch
 from torch.utils.data import Dataset
-import pandas as pd
 import matplotlib.pyplot as plt
+
 
 class SpamDataset(Dataset):
     def __init__(self, csv_file, tokenizer, max_length=None, pad_token_id=50256):
@@ -17,19 +17,15 @@ class SpamDataset(Dataset):
             self.max_length = self._longest_encoded_length()
         else:
             self.max_length = max_length
-            self.encoded_texts = [encoded_text[:self.max_length] for encoded_text in self.encoded_texts]
+            self.encoded_texts = [encoded_text[: self.max_length] for encoded_text in self.encoded_texts]
         self.encoded_texts = [
-            encoded_text + [pad_token_id] * (self.max_length - len(encoded_text))
-            for encoded_text in self.encoded_texts
+            encoded_text + [pad_token_id] * (self.max_length - len(encoded_text)) for encoded_text in self.encoded_texts
         ]
 
     def __getitem__(self, index):
         encoded = self.encoded_texts[index]
         label = self.data.iloc[index]["Label"]
-        return (
-            torch.tensor(encoded, dtype=torch.long),
-            torch.tensor(label, dtype=torch.long)
-        )
+        return (torch.tensor(encoded, dtype=torch.long), torch.tensor(label, dtype=torch.long))
 
     def __len__(self):
         return len(self.data)
@@ -41,44 +37,41 @@ class SpamDataset(Dataset):
             if encoded_length > max_length:
                 max_length = encoded_length
         return max_length
-        
-def download_and_unzip_spam_data(
-        url, zip_path, extracted_path, data_file_path):
+
+
+def download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path):
     if data_file_path.exists():
-        print(f"{data_file_path} already exists. Skipping download "
-              "and extraction."
-        )
+        print(f"{data_file_path} already exists. Skipping download " "and extraction.")
         return
 
-    with urllib.request.urlopen(url) as response:    #1
+    with urllib.request.urlopen(url) as response:  # 1
         with open(zip_path, "wb") as out_file:
             out_file.write(response.read())
 
-    with zipfile.ZipFile(zip_path, "r") as zip_ref:    #2
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:  # 2
         zip_ref.extractall(extracted_path)
 
     original_file_path = Path(extracted_path) / "SMSSpamCollection"
-    os.rename(original_file_path, data_file_path)               #3
+    os.rename(original_file_path, data_file_path)  # 3
     print(f"File downloaded and saved as {data_file_path}")
 
+
 def create_balanced_dataset(df):
-    num_spam = df[df["Label"] == "spam"].shape[0]     #1
-    ham_subset = df[df["Label"] == "ham"].sample(
-        num_spam, random_state=123
-    )                                         #2
-    balanced_df = pd.concat([
-        ham_subset, df[df["Label"] == "spam"]
-    ])                               #3
+    num_spam = df[df["Label"] == "spam"].shape[0]  # 1
+    ham_subset = df[df["Label"] == "ham"].sample(num_spam, random_state=123)  # 2
+    balanced_df = pd.concat([ham_subset, df[df["Label"] == "spam"]])  # 3
     return balanced_df
 
+
 def random_split(df, train_frac, validation_frac):
-    df = df.sample(frac=1, random_state=123).reset_index(drop=True)               #1
-    train_end = int(len(df) * train_frac)          #2
+    df = df.sample(frac=1, random_state=123).reset_index(drop=True)  # 1
+    train_end = int(len(df) * train_frac)  # 2
     validation_end = train_end + int(len(df) * validation_frac)
     train_df = df[:train_end]
     validation_df = df[train_end:validation_end]
     test_df = df[validation_end:]
     return train_df, validation_df, test_df
+
 
 def calc_accuracy_loader(data_loader, model, device, num_batches=None):
     model.eval()
@@ -94,13 +87,11 @@ def calc_accuracy_loader(data_loader, model, device, num_batches=None):
             target_batch = target_batch.to(device)
 
             with torch.no_grad():
-                logits = model(input_batch)[:, -1, :]     #1
+                logits = model(input_batch)[:, -1, :]  # 1
             predicted_labels = torch.argmax(logits, dim=-1)
 
             num_examples += predicted_labels.shape[0]
-            correct_predictions += (
-                (predicted_labels == target_batch).sum().item()
-            )
+            correct_predictions += (predicted_labels == target_batch).sum().item()
 
         else:
             break
@@ -110,52 +101,43 @@ def calc_accuracy_loader(data_loader, model, device, num_batches=None):
 def evaluate_model(model, train_loader, val_loader, device, eval_iter):
     model.eval()
     with torch.no_grad():
-        train_loss = calc_loss_loader(
-            train_loader, model, device, num_batches=eval_iter
-        )
-        val_loss = calc_loss_loader(
-            val_loader, model, device, num_batches=eval_iter
-        )
+        train_loss = calc_loss_loader(train_loader, model, device, num_batches=eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
     model.train()
     return train_loss, val_loss
 
+
 def train_classifier_simple(
-        model, train_loader, val_loader, optimizer, device,
-        num_epochs, eval_freq, eval_iter, tokenizer):
-    train_losses, val_losses, train_accs, val_accs = [], [], [], []   #1
+    model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter, tokenizer
+):
+    train_losses, val_losses, train_accs, val_accs = [], [], [], []  # 1
     examples_seen, global_step = 0, -1
 
-    for epoch in range(num_epochs):    #2
-        model.train()             #3
+    for epoch in range(num_epochs):  # 2
+        model.train()  # 3
 
         for input_batch, target_batch in train_loader:
-            optimizer.zero_grad()                      #4
-            loss = calc_loss_batch(
-                input_batch, target_batch, model, device
-            )
-            loss.backward()                          #5
-            optimizer.step()                          #6
-            examples_seen += input_batch.shape[0]    #7
+            optimizer.zero_grad()  # 4
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()  # 5
+            optimizer.step()  # 6
+            examples_seen += input_batch.shape[0]  # 7
             global_step += 1
 
- #8
+            # 8
             if global_step % eval_freq == 0:
-                train_loss, val_loss = evaluate_model(
-                    model, train_loader, val_loader, device, eval_iter)
+                train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
                 train_losses.append(train_loss)
                 val_losses.append(val_loss)
-                print(f"Ep {epoch+1} (Step {global_step:06d}): "
-                      f"Train loss {train_loss:.3f}, "
-                      f"Val loss {val_loss:.3f}"
+                print(
+                    f"Ep {epoch+1} (Step {global_step:06d}): "
+                    f"Train loss {train_loss:.3f}, "
+                    f"Val loss {val_loss:.3f}"
                 )
 
- #9
-        train_accuracy = calc_accuracy_loader(
-            train_loader, model, device, num_batches=eval_iter
-        )
-        val_accuracy = calc_accuracy_loader(
-            val_loader, model, device, num_batches=eval_iter
-        )
+        # 9
+        train_accuracy = calc_accuracy_loader(train_loader, model, device, num_batches=eval_iter)
+        val_accuracy = calc_accuracy_loader(val_loader, model, device, num_batches=eval_iter)
 
         print(f"Training accuracy: {train_accuracy*100:.2f}% | ", end="")
         print(f"Validation accuracy: {val_accuracy*100:.2f}%")
@@ -164,54 +146,50 @@ def train_classifier_simple(
 
     return train_losses, val_losses, train_accs, val_accs, examples_seen
 
+
 def calc_loss_batch(input_batch, target_batch, model, device):
     input_batch = input_batch.to(device)
     target_batch = target_batch.to(device)
-    logits = model(input_batch)[:, -1, :]     #1
+    logits = model(input_batch)[:, -1, :]  # 1
     loss = torch.nn.functional.cross_entropy(logits, target_batch)
     return loss
 
+
 def calc_loss_loader(data_loader, model, device, num_batches=None):
-    total_loss = 0.
+    total_loss = 0.0
     if len(data_loader) == 0:
         return float("nan")
     elif num_batches is None:
         num_batches = len(data_loader)
-    else:                                        #1
+    else:  # 1
         num_batches = min(num_batches, len(data_loader))
     for i, (input_batch, target_batch) in enumerate(data_loader):
         if i < num_batches:
-            loss = calc_loss_batch(
-                input_batch, target_batch, model, device
-            )
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
             total_loss += loss.item()
         else:
             break
     return total_loss / num_batches
 
 
-
-def plot_values(
-        epochs_seen, examples_seen, train_values, val_values,
-        label="loss"):
+def plot_values(epochs_seen, examples_seen, train_values, val_values, label="loss"):
     fig, ax1 = plt.subplots(figsize=(5, 3))
 
- #1
+    # 1
     ax1.plot(epochs_seen, train_values, label=f"Training {label}")
-    ax1.plot(
-        epochs_seen, val_values, linestyle="-.",
-        label=f"Validation {label}"
-    )
+    ax1.plot(epochs_seen, val_values, linestyle="-.", label=f"Validation {label}")
     ax1.set_xlabel("Epochs")
     ax1.set_ylabel(label.capitalize())
     ax1.legend()
 
- #2
+    # 2
     ax2 = ax1.twiny()
-    ax2.plot(examples_seen, train_values, alpha=0)    #3
+    ax2.plot(examples_seen, train_values, alpha=0)  # 3
     ax2.set_xlabel("Examples seen")
 
-    fig.tight_layout()             #4
+    fig.tight_layout()  # 4
     plt.savefig(f"{label}-plot.pdf")
     plt.show()
-#EOF
+
+
+# EOF
